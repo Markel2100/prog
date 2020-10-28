409391028e209155abc901e5aca66440fc2bb300
Markel2100 <73583169+Markel2100@users.noreply.github.com>
2020-10-28 10:09:46 +0100

    Add files via upload

---
+++ BATUBIKOITIW.java
@@ -0,0 +1,31 @@
+package Eb1;
+import java.util.Scanner;
+
+public class BATUBIKOITIW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ingurunea
+		int znb=0;
+		int znb1=0;
+		int aku=0;
+		Scanner teklatua=new Scanner(System.in);
+		System.out.println("sartu zenbaki bat");
+		znb=teklatua.nextInt();
+		
+		while (znb>znb1) {
+		aku=aku+znb1;
+		znb1++;
+		}
+			
+		System.out.println("gehiketa " +aku+ " da");
+		teklatua.close();
+			
+			//eguneraketa
+		}
+		
+		
+		
+		
+	}
+
